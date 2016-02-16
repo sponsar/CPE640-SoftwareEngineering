d1ded684daa73f54a814d4607ef1a7ff892b4880
Xinyao Huang <406665325@qq.com>
2016-02-15 17:11:33 -0800

    Create LinkList-mynode.java

---
+++ LinkList-mynode.java
@@ -0,0 +1,15 @@
+package linklist;
+
+public class mynode {
+	public int value;
+	public mynode nextnode;
+	
+	public mynode(){
+		nextnode = null;
+	}
+	
+	public mynode(int v){
+		value = v;
+		nextnode = null;
+	}
+}
